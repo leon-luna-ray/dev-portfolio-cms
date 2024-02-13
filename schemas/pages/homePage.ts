b1e324fd0b75c62a1e3716e002635f102b678ea5
Ray Luna <leon.luna.ray@gmail.com>
2024-02-12 19:32:27 -0800

    update home page schema

--- schemas/pages/homePage.ts
+++ schemas/pages/homePage.ts
@@ -67,6 +67,33 @@ export default defineType({
       ],
     }),
     defineField({
+      name: 'introSection',
+      title: 'Intro Section',
+      type: 'object',
+      fields: [
+        defineField({
+          name: 'title',
+          title: 'Section Title',
+          type: 'string',
+        }),
+        defineField({
+          name: 'subtitle',
+          title: 'Section Subtitle',
+          type: 'string',
+        }),
+        defineField({
+          name: 'intro',
+          title: 'Section Intro',
+          type: 'text',
+        }),
+        defineField({
+          name: 'body',
+          title: 'Section Body',
+          type: 'blockContent',
+        }),
+      ],
+    }),
+    defineField({
       name: 'aboutSection',
       title: 'About Section',
       type: 'object',
